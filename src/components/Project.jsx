8615a2ba2b445270895312a03af0e4bc0f935c55
Claire Joy M. Magno <clairey04magno@gmail.com>
2026-02-03 21:41:49 +0800

    Added new animation on Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,19 +8,22 @@ const Project = () =>{
             <h1 className="font-sans font-bold flex text-center justify-center text-2xl w-50 h-10 m-auto rounded-full bg-white"><span className="text-center text-lg pt-2 mr-2"><FaDna /></span>Projects</h1>
                <div className="grid grid-cols-3 justify-items-center pt-6">
 
-                    <div className="w-70 h-100 bg-blue-300 rounded-xl">
+                    <div className="w-70 h-100 hover bg-gray-300 card-hover rounded-xl shadow-xl">
                         <img className="m-auto w-full rounded-t-lg h-52" src="/profile.jpg" alt="pic 1" />
+
                         <h1 className="heading-title">Title</h1>
                         <p className="px-4 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cumque distinctio enim facilis fuga quis quod reprehenderit voluptatibus? Consequatur esse </p>
+
                     </div>
 
-                    <div className="w-70 h-100 bg-blue-300 rounded-xl">
+
+                    <div className="w-70 h-100 bg-gray-300 card-hover rounded-xl shadow-xl">
                         <img className="m-auto w-full  rounded-t-lg h-52" src="/profile.jpg" alt="pic 1" />
                         <h1 className="heading-title">Title</h1>
                         <p className="px-4 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cumque distinctio enim facilis fuga quis quod reprehenderit voluptatibus? Consequatur esse </p>
                     </div>
 
-                   <div className="w-70 h-100 bg-blue-300 rounded-xl">
+                   <div className="w-70 h-100 bg-gray-300 card-hover rounded-xl shadow-xl">
                        <img className="m-auto w-full  rounded-t-lg h-52" src="/profile.jpg" alt="pic 1" />
                        <h1 className="heading-title">Title</h1>
                        <p className="px-4 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cumque distinctio enim facilis fuga quis quod reprehenderit voluptatibus? Consequatur esse </p>
